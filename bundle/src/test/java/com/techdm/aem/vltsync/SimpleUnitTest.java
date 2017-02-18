0408ecf4b94789b36f6b3c94946e205e08c9aaa9
Daniel Henrique Alves Lima <daniel@danielhalima.com>
2017-02-18 07:07:31 -0200

    Initial structure created by multimodule-content-package-archetype

---
+++ bundle/src/test/java/com/techdm/aem/vltsync/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.techdm.aem.vltsync;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
